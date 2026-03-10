d127af2f2867d1c394e536a073370a35d3a5e09f
Christ Tchuidjang <christnehemie237@gmail.com>
2026-03-10 10:47:11 +0100

    la structure

---
+++ backend/service-utilisateurs/src/test/java/com/agriconnect/service_utilisateurs/ServiceUtilisateursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriconnect.service_utilisateurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUtilisateursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
